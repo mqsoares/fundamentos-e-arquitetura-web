307cff4dc46e44323cb0fedd53a6f647e4173bfe
Miquéias Soares <mqseraos@gmail.com>
2024-02-07 00:30:40 -0300

    Create and Refactore Page Characters

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Navbar } from "./components/nav-bar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { CharactersCard } from "./pages";
 
 function App() {
     return (
         <>
             <Navbar />
+            <CharactersCard />
         </>
     );
 }
