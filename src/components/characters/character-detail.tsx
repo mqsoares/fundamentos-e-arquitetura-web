08295b764327c4216cab841e1acbd9f595e9b2f2
Miquéias Soares <mqseraos@gmail.com>
2024-02-08 01:54:34 -0300

    Adapt responsivity and format str episodes

--- src/components/characters/character-detail.tsx
+++ src/components/characters/character-detail.tsx
@@ -1,16 +1,22 @@
 import { Badge, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import { IResult } from "../types";
 
 export const CharactersDetail = (character: IResult) => {
+    const onlyNumberEpisode = (str: string) => {
+        const strFormat = str.replace(/[^0-9]/g, " ");
+        return strFormat;
+    };
+
     return (
         <section className="my-4 d-flex flex-column">
-            <Card className=" p-4 d-flex flex-row justify-content-evenly">
+            <Card className="p-4 d-flex flex-lg-row justify-content-evenly align-items-center">
                 <Card.Img
                     style={{ maxWidth: "25rem", borderRadius: "1rem" }}
                     src={character?.image}
                 />
-                <div className="detail-characters fs-4 d-flex flex-column  justify-content-evenly">
+                <div className="fs-4 d-flex flex-column justify-content-evenly ">
                     <div className="fs-1 fw-bold">
                         {character?.name} &#160;
                         <Badge
@@ -46,15 +52,26 @@ export const CharactersDetail = (character: IResult) => {
                     </div>
                 </div>
             </Card>
-            <div className="pt-4 text-center">
-                <p className="fs-3 ">
+            <div className="pt-4">
+                <p className="fs-3 text-center">
                     Lista de Episódios com &#34;
                     {character?.name}&#34;
                 </p>
-                {/* eslint-disable-next-line no-undef */}
-                {character?.episode.map<React.ReactNode>((el, index) => (
-                    <li key={index}>{el}</li>
-                ))}
+                <div className="row text-center">
+                    {/* eslint-disable-next-line no-undef */}
+                    {character?.episode.map<React.ReactNode>((el, index) => (
+                        <div key={index} className="col-sm-6 col-md-4 col-lg-2">
+                            <Link className="nav-link nav-link-ep fs-4" to="#">
+                                <p
+                                    className=""
+                                    style={{ whiteSpace: "nowrap" }}
+                                >
+                                    Episódio: {onlyNumberEpisode(el)}
+                                </p>
+                            </Link>
+                        </div>
+                    ))}
+                </div>
             </div>
         </section>
     );
