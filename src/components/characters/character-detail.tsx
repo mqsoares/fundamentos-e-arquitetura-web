21d74bfd31f0f73b3628d342cfe7bffde70d802c
Miquéias Soares <mqseraos@gmail.com>
2024-02-07 23:28:44 -0300

    Component characters detail

--- src/components/characters/character-detail.tsx
+++ src/components/characters/character-detail.tsx
@@ -1,3 +1,61 @@
-export const CharactersDetail = () => {
-    return <>teste</>;
+import { Badge, Card } from "react-bootstrap";
+
+import { IResult } from "../types";
+
+export const CharactersDetail = (character: IResult) => {
+    return (
+        <section className="my-4 d-flex flex-column">
+            <Card className=" p-4 d-flex flex-row justify-content-evenly">
+                <Card.Img
+                    style={{ maxWidth: "25rem", borderRadius: "1rem" }}
+                    src={character?.image}
+                />
+                <div className="detail-characters fs-4 d-flex flex-column  justify-content-evenly">
+                    <div className="fs-1 fw-bold">
+                        {character?.name} &#160;
+                        <Badge
+                            bg={
+                                character?.status != "Alive"
+                                    ? character?.status != "Dead"
+                                        ? "secondary"
+                                        : "danger"
+                                    : "success"
+                            }
+                            style={{
+                                maxWidth: "12rem",
+                            }}
+                            className=""
+                        >
+                            {character?.status}
+                        </Badge>
+                    </div>
+                    <p>
+                        <span className="text-secondary">Espécie:</span>{" "}
+                        {character?.species}
+                    </p>
+                    <p>
+                        <span className="text-secondary">Origem:</span>{" "}
+                        {character?.origin.name}
+                    </p>
+                    <div>
+                        <span className="text-secondary">
+                            Last known location:
+                        </span>
+                        <br />
+                        {character?.location.name}
+                    </div>
+                </div>
+            </Card>
+            <div className="pt-4 text-center">
+                <p className="fs-3 ">
+                    Lista de Episódios com &#34;
+                    {character?.name}&#34;
+                </p>
+                {/* eslint-disable-next-line no-undef */}
+                {character?.episode.map<React.ReactNode>((el, index) => (
+                    <li key={index}>{el}</li>
+                ))}
+            </div>
+        </section>
+    );
 };
---
+++ src/pages/character-detail/character.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+
+import { CharactersDetail, IResult } from "../../components";
+import { getCharacter } from "../../services";
+
+export const Character = () => {
+    const [character, setCharacter] = useState<IResult>();
+    const [isLoad, setIsload] = useState(false);
+
+    const { id } = useParams();
+
+    useEffect(() => {
+        const fetchCharacter = async () => {
+            try {
+                setIsload(true);
+                const data = await getCharacter(id as string);
+                setCharacter(data);
+            } catch (error) {
+                // eslint-disable-next-line no-console
+                console.error(error);
+            } finally {
+                setIsload(false);
+            }
+        };
+        fetchCharacter();
+    }, []);
+
+    return (
+        <Container className="wrap-container">
+            {isLoad && <h1>Carregando...</h1>}
+            {character ? (
+                <CharactersDetail
+                    id={character.id}
+                    image={character.image}
+                    name={character.name}
+                    status={character.status}
+                    species={character.species}
+                    origin={character.origin}
+                    location={character.location}
+                    episode={character.episode}
+                    type={character.type}
+                    gender={character.gender}
+                    url={character.url}
+                    created={character.created}
+                />
+            ) : (
+                <div>
+                    <h2>Personagem não encontrado(a)</h2>
+                </div>
+            )}
+        </Container>
+    );
+};
