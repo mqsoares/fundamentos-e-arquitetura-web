79ca0240d1d0f24041ae01eb7c77dd49d8c67974
Miquéias Soares <mqseraos@gmail.com>
2024-02-18 14:51:05 -0300

    Rename character-detail to home-character-detail

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,5 @@
 export * from "./home";
 export * from "./episodes";
 export * from "./location";
-export * from "./character-detail";
+export * from "./home-character-detail";
 export * from "./episodes-detail";
