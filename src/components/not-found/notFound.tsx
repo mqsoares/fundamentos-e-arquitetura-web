24cd0f175901f701f1c616ccd47485fcea762aea
Miquéias Soares <mqseraos@gmail.com>
2024-02-18 14:49:26 -0300

    Create notFound component

---
+++ src/components/not-found/notFound.tsx
@@ -0,0 +1,12 @@
+export const NotFound = () => {
+    return (
+        <div
+            className="d-flex flex-column justify-content-center align-items-center"
+            style={{ height: "85vh" }}
+        >
+            <h1>Oops!</h1>
+            <h2>404 - Não Encontramos o que você solicitou.</h2>
+            <p>Verifique a conexão de sua internet e tente novamente.</p>
+        </div>
+    );
+};
