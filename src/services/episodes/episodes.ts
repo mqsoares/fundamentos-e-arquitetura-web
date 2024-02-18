0507d2d427eee3e417abd85f9219d74e56b3e9ea
Miquéias Soares <mqseraos@gmail.com>
2024-02-18 17:28:41 -0300

    Create getEpisodePath service

--- src/services/episodes/episodes.ts
+++ src/services/episodes/episodes.ts
@@ -9,3 +9,8 @@ export const getEpisode = async (id: string) => {
     const { data } = await api.get(`episode/${id}`);
     return data;
 };
+
+export const getEpisodePath = async (pathURL: string) => {
+    const { data } = await api.get(`${pathURL}`);
+    return data;
+};
