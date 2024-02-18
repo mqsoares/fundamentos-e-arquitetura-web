7ca55d2886ba4c97065f105898549abd4dcc7bb1
Miquéias Soares <mqseraos@gmail.com>
2024-02-18 12:47:31 -0300

    Adapt title btn

--- src/components/btn-back/btn-back.tsx
+++ src/components/btn-back/btn-back.tsx
@@ -1,13 +1,8 @@
-import { useContext } from "react";
-
-import { NumPageContext } from "../../context/num-page-context";
-
 export const BtnBack = () => {
-    const { numPage } = useContext(NumPageContext);
     return (
         <nav className="pt-4">
             <button onClick={() => history.back()} className="nav-link">
-                Voltar para Pág. {numPage}
+                Voltar
             </button>
         </nav>
     );
