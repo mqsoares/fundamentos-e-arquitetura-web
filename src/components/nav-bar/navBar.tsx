f7d947735b2864459bec317015cc9da6842dc080
Miquéias Soares <mqseraos@gmail.com>
2024-02-19 17:36:18 -0300

    Ajust theme dark

--- src/components/nav-bar/navBar.tsx
+++ src/components/nav-bar/navBar.tsx
@@ -4,31 +4,30 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
     return (
         <BoostrapNavbar expand="lg" className="bg-body-tertiary">
-            <Container className="align-items-baseline">
+            <Container className="align-items-center">
                 <BoostrapNavbar.Brand href="/">
                     <div className="d-flex flex-row align-items-baseline gap-2">
                         <img
-                            src="/rickmortylogo.svg"
+                            src="/r&mlogonav.png"
                             alt="logo rick and morty"
-                            width="40px"
+                            width="180px"
                         ></img>
-                        <span className="fs-3">Rick and Morty</span>
                     </div>
                 </BoostrapNavbar.Brand>
                 <BoostrapNavbar.Toggle aria-controls="basic-navbar-nav" />
                 <BoostrapNavbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Link className="fs-5 nav-link" to="/">
+                        <Link className="fs-4 nav-link" to="/">
                             {" "}
                             Personagens
                         </Link>
 
-                        <Link className="fs-5 nav-link" to="episodes">
+                        <Link className="fs-4 nav-link" to="episodes">
                             {" "}
                             Episódios
                         </Link>
 
-                        <Link className="fs-5 nav-link" to="location">
+                        <Link className="fs-4 nav-link" to="location">
                             {" "}
                             Localização
                         </Link>
